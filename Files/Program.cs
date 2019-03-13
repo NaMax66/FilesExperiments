2b4a4c4e62dd02d86a655332e48d39cd479537eb
Максим Найдович <max.naid@mail.ru>
2019-03-13 19:17:36 +0500

    Выделил проверку имени в отдельный метод

--- Files/Program.cs
+++ Files/Program.cs
@@ -23,9 +23,19 @@ namespace Files
 
         private static void CopyFileUsingFileClass(string sorceFile, string newFile)
         {
-            //string[] lines = File.ReadAllLines(sorceFile, Encoding.Default);
 
+            string finalFileName = DefineFileName(newFile);
+
+            
+            File.Copy(sorceFile, finalFileName);
+
+            Console.WriteLine(string.Format("file {0} has been copied to file {1}",sorceFile, finalFileName));
+        }
+
+        private static string DefineFileName(string newFile)
+        {
             int i = 1; //it needs for new files names
+
             string fileExtention = Path.GetExtension(newFile);
             string orginalFileName = newFile.Remove(newFile.Length - fileExtention.Length);
             string finalFileName = newFile;
@@ -35,10 +45,7 @@ namespace Files
                 finalFileName = orginalFileName + i + fileExtention;
                 i++;
             }
-            File.Copy(sorceFile, finalFileName);
-            Console.WriteLine(string.Format("file {0} has been copied to file {1}",sorceFile, finalFileName));
+            return finalFileName;
         }
-
-      
     }
 }
